e1af9a83e9e96c475e6de0261051816f9994a86f
Guilherme <guilherme.devsoft@gmail.com>
2025-12-05 00:30:30 -0300

    projeto criado e adicionado aula de vetores

---
+++ src/estruturadedados/Vetor.java
@@ -0,0 +1,10 @@
+package estruturadedados;
+
+public class Vetor {
+
+    private String[] elementos;
+
+    public Vetor(int capacidade) {
+        this.elementos = new String[capacidade];
+    }
+}
