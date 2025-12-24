8ac0d684d4b9ccc85df0fb6b6fe31f4f7fcc771a
Guilherme <guilherme.devsoft@gmail.com>
2025-12-24 00:11:03 -0300

    verificando elemento do topo

---
+++ src/teste/pilha/AulaPilha16.java
@@ -0,0 +1,22 @@
+package teste.pilha;
+
+import estruturadedados.pilhas.Pilha;
+
+public class AulaPilha16 {
+    public static void main(String[] args) {
+
+        Pilha<Integer> pilha = new Pilha<Integer>();
+
+        System.out.println(pilha.topo());
+
+        System.out.println(pilha);
+
+        pilha.empilha(1);
+        pilha.empilha(2);
+        pilha.empilha(3);
+
+        System.out.println(pilha.topo());
+
+        System.out.println(pilha);
+    }
+}
