70203c0fd75a5929e2c9090469cbda8c99a4ab75
Guilherme <guilherme.devsoft@gmail.com>
2025-12-26 20:46:19 -0300

    filas

---
+++ src/estruturadedados/filas/Fila.java
@@ -0,0 +1,16 @@
+package estruturadedados.filas;
+
+import estruturadedados.EstruturaEstatica;
+
+public class Fila extends EstruturaEstatica {
+
+    public Fila(){
+        super();
+    }
+
+    public Fila(int capacidade){
+        super(capacidade);
+    }
+
+
+}
